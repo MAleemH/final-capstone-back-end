4ead8d7055f44e90c67f9b6413e2619bcd34a5c8
fitz95 <fitzgeraldnsong@gmail.com>
2023-06-12 13:48:38 +0100

    fix linter issues

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,12 +1,12 @@
 class Therapist < ApplicationRecord
-    belongs_to :user
-    has_many :appointments, dependent: :destroy
+  belongs_to :user
+  has_many :appointments, dependent: :destroy
 
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :specialization, presence: true
-    validates :phone, presence: true
-    validates :photo, presence: true
-    validates :availability, presence: true
-    validates :address, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :specialization, presence: true
+  validates :phone, presence: true
+  validates :photo, presence: true
+  validates :availability, presence: true
+  validates :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_many :appointments dependent: :destroy
-    has_many :therapists
+  has_many :appointments, dependent: :destroy
+  has_many :therapists
 
-    validates :name, presence: true uniqueness: true
-    validates :email, presence: true
-    validates :role, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :role, presence: true
 end
