eee248537211731563f59c13141038a392181622
MAleemH <aleemhashmi4321@gmail.com>
2023-06-12 20:33:54 +0500

    Index method added in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+    def index
+        @users = User.all
+        if @users.empty?
+            render json:{ error: "Users not found!" }, status: 404
+        else
+            render json: @users
+        end
+    end
 end
