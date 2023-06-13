a4ff59cfe7593fa2b6d6b181dbfcba8e2b1f6824
MAleemH <aleemhashmi4321@gmail.com>
2023-06-13 13:38:37 +0500

    Rubocop error fixed

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,43 +1,43 @@
 class Api::V1::AppointmentsController < ApplicationController
-    def index
-        @appointments = Appointment.all
-        if @appointments.empty?
-            render json: { error: @appointments.errors.full_messages }, status: :unprocessable_entity
-        else
-            render json: @appointments
-        end
+  def index
+    @appointments = Appointment.all
+    if @appointments.empty?
+      render json: { error: @appointments.errors.full_messages }, status: :unprocessable_entity
+    else
+      render json: @appointments
     end
-    
-    def show
-        @appointment = Appointment.find_by(id: params[id])
-        if @appointment.present?
-            render json: @appointment
-        else
-            render json: { error: @appointment.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
+  end
 
-    def create
-        @appointment = Appointment.new(appointment_params)
-        if @appointment.save
-            render json: @appointment, status: :created
-          else
-            render json: @appointment.errors, status: :unprocessable_entity
-        end
+  def show
+    @appointment = Appointment.find_by(id: params[id])
+    if @appointment.present?
+      render json: @appointment
+    else
+      render json: { error: @appointment.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-        @appointment = Appointment.find_by(id: params[:id])
-        if @appointment.destroy
-            render json: { message: "Appointment deleted successfully!" }, status: :ok
-        else
-            render json: { error: @appointment.errors.full_messages }, status: :unprocessable_entity
-        end
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      render json: @appointment, status: :created
+    else
+      render json: @appointment.errors, status: :unprocessable_entity
     end
+  end
 
-    private
-
-    def appointment_params
-        params.require(:appointment).permit(:date, :status, :user_id, :therapist_id)
+  def destroy
+    @appointment = Appointment.find_by(id: params[:id])
+    if @appointment.destroy
+      render json: { message: 'Appointment deleted successfully!' }, status: :ok
+    else
+      render json: { error: @appointment.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:date, :status, :user_id, :therapist_id)
+  end
 end
--- app/controllers/api/v1/therapists_controller.rb
+++ app/controllers/api/v1/therapists_controller.rb
@@ -1,43 +1,43 @@
 class Api::V1::TherapistsController < ApplicationController
-    def index
-        @therapists = Therapist.all
-        if @therapists.empty?
-            render json:{ error: @therapists.errors.full_messages }, status: :unprocessable_entity
-        else
-            render json: @therapists
-        end
+  def index
+    @therapists = Therapist.all
+    if @therapists.empty?
+      render json: { error: @therapists.errors.full_messages }, status: :unprocessable_entity
+    else
+      render json: @therapists
     end
+  end
 
-    def show
-        @therapist = Therapist.find_by(id: params[id])
-        if @therapist.present?
-            render json: @therapist
-        else
-            render json: { error: "Therapist doesn't found!"}, status: 404
-        end
+  def show
+    @therapist = Therapist.find_by(id: params[id])
+    if @therapist.present?
+      render json: @therapist
+    else
+      render json: { error: "Therapist doesn't found!" }, status: 404
     end
+  end
 
-    def create
-        @therapist = Therapist.new(therapist_params)
-        if @therapist.save
-            render json: @therapist, status: created
-        else
-            render json: @therapist.errors, status: :unprocessable_entity
-        end
+  def create
+    @therapist = Therapist.new(therapist_params)
+    if @therapist.save
+      render json: @therapist, status: created
+    else
+      render json: @therapist.errors, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-        @therapist = Therapist.find_by(id: params[:id])
-        if @therapist.destroy
-            render json: { message: "Therapist deleted successfully!" }, status: :ok
-        else
-            render json: { error: @therapist.errors.full_messages }, status: :unprocessable_entity
-        end
+  def destroy
+    @therapist = Therapist.find_by(id: params[:id])
+    if @therapist.destroy
+      render json: { message: 'Therapist deleted successfully!' }, status: :ok
+    else
+      render json: { error: @therapist.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    private
+  private
 
-    def therapist_params
-        params.require(:therapist).permit(:name, :email, :specialization, :phone, :photo, :availability, :address, :user_id)
-    end
+  def therapist_params
+    params.require(:therapist).permit(:name, :email, :specialization, :phone, :photo, :availability, :address, :user_id)
+  end
 end
