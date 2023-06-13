f73b8d8aa5857f90b8a50f50eeb89e02ef030cca
MAleemH <aleemhashmi4321@gmail.com>
2023-06-13 13:18:57 +0500

    Added index method in appointments controller

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::AppointmentsController < ApplicationController
+    def index
+        @appointments = Appointment.all
+        if @appointments.empty?
+            render json: { error: @appointments.errors.full_messages }, status: :unprocessable_entity
+        else
+            render json: @appointments
+        end
+    end
 end
