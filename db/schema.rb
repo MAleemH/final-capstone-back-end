43a1ab7c035146d72e183a340e714aff8d0d35f1
zewdu erkyhun <zewdu444@gmail.com>
2023-06-13 13:17:30 +0300

    Migrate token revokation table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_092925) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_094511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_092925) do
     t.datetime "updated_at", null: false
     t.index ["therapist_id"], name: "index_appointments_on_therapist_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "revoked_tokens", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_revoked_tokens_on_token"
   end
 
   create_table "therapists", force: :cascade do |t|
