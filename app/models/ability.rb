44cfed37ab5294bf330dc0144a6a898554da5185
zewdu erkyhun <zewdu444@gmail.com>
2023-06-13 17:44:14 +0300

    Fix linter error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,19 +1,19 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-       return unless user.present?
-       can :read, :all
-       can :create, Appointment
-       can :destroy, Appointment
+    return unless user.present?
 
-       if user.role == 'admin'
-          can:manage, :all
-       end
+    can :read, :all
+    can :create, Appointment
+    can :destroy, Appointment
+
+    return unless user.role == 'admin'
+
+    can :manage, :all
+
 
     #   return unless user.admin?
     #   can :manage, :all
