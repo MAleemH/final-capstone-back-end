974720158722d0e5aa09c7ed27befaabbab934d1
MAleemH <aleemhashmi4321@gmail.com>
2023-06-13 12:48:11 +0500

    Index method added in therapists controller

--- app/controllers/api/v1/therapists_controller.rb
+++ app/controllers/api/v1/therapists_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::TherapistsController < ApplicationController
+    def index
+        @therapists = Therapist.all
+        if @therapists.empty?
+            render json:{ error: @therapists.errors.full_messages }, status: :unprocessable_entity
+        else
+            render json: @therapists
+        end
+    end
 end
