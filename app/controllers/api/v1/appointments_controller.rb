2ffd7d8068f987c0d4ea17c9ffc6a3011ae9fa3b
MAleemH <aleemhashmi4321@gmail.com>
2023-06-13 13:21:25 +0500

    Added show method in appointments controller

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -7,4 +7,13 @@ class Api::V1::AppointmentsController < ApplicationController
             render json: @appointments
         end
     end
+    
+    def show
+        @appointment = Appointment.find_by(id: params[id])
+        if @appointment.present?
+            render json: @appointment
+        else
+            render json: { error: @appointment.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
 end
