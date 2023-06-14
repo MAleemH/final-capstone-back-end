6144f179d6d1cd176edbd70bdb3767123bc58902
fitz95 <fitzgeraldnsong@gmail.com>
2023-06-14 12:59:38 +0200

    test validation for name

--- spec/models/api/v1/user_spec.rb
+++ spec/models/api/v1/user_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe User, type: :model do
    it "is valid with valid attributes" do
       expect(@user).to be_valid
    end
-   
+    it "is not valid without a name" do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+    
 end
