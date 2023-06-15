c7cbb982c924dbe5e1a6f29d89d4a208b5e1877c
fitz95 <fitzgeraldnsong@gmail.com>
2023-06-15 09:37:34 +0100

    set up therapist endpoint test

--- spec/requests/api/v1/therapists_spec.rb
+++ spec/requests/api/v1/therapists_spec.rb
@@ -1,7 +1,37 @@
 require 'rails_helper'
-
+​
 RSpec.describe 'Api::V1::Therapists', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before(:each) do
+    @user =  User.create!(
+      name: Faker::Name.unique.name,
+      email: Faker::Internet.unique.email,
+      password: "123456",
+      password_confirmation: "123456",
+      role: 'admin'
+     )
+     @therapist = Therapist.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.unique.email,
+      specialization: Faker::Job.title,
+      phone: Faker::PhoneNumber.unique.cell_phone,
+      photo: Faker::Internet.url,
+      availability: Faker::Boolean.boolean,
+      address: Faker::Address.full_address,
+      user_id: @user.id
+    )
+    post '/api/v1/users/sign_in', params: { user: {email: @user.email, password: @user.password } }
+    @token = response.headers['Authorization']
   end
+  describe 'therapist endpoints' do
+     it 'returns all therapists' do
+       get "/api/v1/users/#{@user.id}/therapists", headers: { Authorization: @token }
+       expect(response).to have_http_status(:success)
+     end
+      it 'returns error message if token is not valid' do
+         token = nil
+         get "/api/v1/users/#{@user.id}/therapists", headers: { Authorization: token }
+          expect(response).to have_http_status(:unauthorized)
+      end
+  end
+  
 end
