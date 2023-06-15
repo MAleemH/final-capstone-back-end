84b7f446ba47e115ea0ac0bf7a60e8c090aa200c
fitz95 <fitzgeraldnsong@gmail.com>
2023-06-15 09:40:05 +0100

    test for therapist create endpoint

--- spec/requests/api/v1/therapists_spec.rb
+++ spec/requests/api/v1/therapists_spec.rb
@@ -44,5 +44,31 @@ RSpec.describe 'Api::V1::Therapists', type: :request do
           expect(response).to have_http_status(:unauthorized)
       end
   end
+  describe 'therapist create endpoints' do
+      it 'return successful message if user is admin' do
+        @user.update(role: 'admin')
+        post "/api/v1/users/#{@user['id']}/therapists", params: { therapist: { name: Faker::Name.name,
+          email: Faker::Internet.unique.email,
+          specialization: Faker::Job.title,
+          phone: Faker::PhoneNumber.unique.cell_phone,
+          photo: Faker::Internet.url,
+          availability: Faker::Boolean.boolean,
+          address: Faker::Address.full_address,
+          user_id: @user.id } }, headers: { Authorization: @token }
+        expect(response).to have_http_status(:success)
+      end
+      it 'returns error message if token is not valid' do
+          token = nil
+          post "/api/v1/users/#{@user['id']}/therapists", params: { therapist: { name: Faker::Name.name,
+            email: Faker::Internet.unique.email,
+            specialization: Faker::Job.title,
+            phone: Faker::PhoneNumber.unique.cell_phone,
+            photo: Faker::Internet.url,
+            availability: Faker::Boolean.boolean,
+            address: Faker::Address.full_address,
+            user_id: @user.id } }, headers: { Authorization: token }
+            expect(response).to have_http_status(:unauthorized)
+      end
+  end
   
 end
