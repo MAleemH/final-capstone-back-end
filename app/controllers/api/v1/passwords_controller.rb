a4147903d2bbfc8a8237598dfbe764621cd636d3
MAleemH <aleemhashmi4321@gmail.com>
2023-06-15 21:29:10 +0500

    Added passwords controller api documentation

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::PasswordsController < Devise::PasswordsController
   respond_to :json
   # PUT  /api/v1/password
+  api :PUT, '/v1/users/password', 'Update user password'
+  param :user, Hash, desc: 'User info', required: true do
+    param :email, String, desc: 'User email', required: true
+    param :password, String, desc: 'User password', required: true
+    param :password_confirmation, String, desc: 'User password confirmation', required: true
+  end
+  error code: 404, desc: 'Password not updated!'
   def update
     self.resource = resource_class.find_by(email: resource_params[:email])
 
