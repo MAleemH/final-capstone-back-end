36f3e8274bb47eb370fa095237327b37f0053096
zewdu erkyhun <zewdu444@gmail.com>
2023-06-15 19:58:48 +0300

    Migrate test database

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::PasswordsController < Devise::PasswordsController
     self.resource = resource_class.find_by(email: resource_params[:email])
 
     if resource && resource.update(resource_params)
-      sign_in(resource, bypass: true) # Manually sign in the user
+      bypass_sign_in(resource) # Manually sign in the user
       render json: { message: 'Your password has been successfully reset' }, status: :ok
     else
       render json: { error: 'Invalid user or password confirmation' }, status: :unprocessable_entity
