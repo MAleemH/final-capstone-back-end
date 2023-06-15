9376db476a837f1031c927e7084d9ae0539113dc
fitz95 <fitzgeraldnsong@gmail.com>
2023-06-15 09:42:58 +0100

    setup appointment endpoint test

--- spec/requests/api/v1/appointments_spec.rb
+++ spec/requests/api/v1/appointments_spec.rb
@@ -1,10 +1,41 @@
 require 'rails_helper'
-
+​
 RSpec.describe 'Api::V1::Appointments', type: :request do
-  describe 'GET /–-api' do
-    it 'returns http success' do
-      get '/api/v1/appointments/–-api'
-      expect(response).to have_http_status(:success)
+   before(:each) do
+    @user =  User.create!(
+      name: Faker::Name.unique.name,
+      email: Faker::Internet.unique.email,
+      password: "123456",
+      password_confirmation: "123456",
+      role: 'admin'
+     )
+     @therapist = Therapist.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.unique.email,
+      specialization: Faker::Job.title,
+      phone: Faker::PhoneNumber.unique.cell_phone,
+      photo: Faker::Internet.url,
+      availability: Faker::Boolean.boolean,
+      address: Faker::Address.full_address,
+      user_id: @user.id
+    )
+    @appointment = Appointment.create!(
+      date: Faker::Date.forward(days: 23),
+      user_id: @user.id,
+      therapist_id: @therapist.id)
+      post '/api/v1/users/sign_in', params: { user: {email: @user.email, password: @user.password } }
+      @token = response.headers['Authorization']
+   end
+    describe 'appointment endpoints' do
+       it 'returns all appointments' do
+          get "/api/v1/users/#{@user['id']}/appointments", headers: { Authorization: @token }
+          expect(response).to have_http_status(:success)
+       end
+        it 'returns error message if token is not valid' do
+            token = nil
+            get "/api/v1/users/#{@user['id']}/appointments", headers: { Authorization: token }
+            expect(response).to have_http_status(:unauthorized)
+        end
     end
-  end
+    
 end
