3741baac38004e528765f227fb647993dbd5a3cb
fitz95 <fitzgeraldnsong@gmail.com>
2023-06-15 09:47:39 +0100

    add test for error message token

--- spec/requests/api/v1/appointments_spec.rb
+++ spec/requests/api/v1/appointments_spec.rb
@@ -48,5 +48,21 @@ RSpec.describe 'Api::V1::Appointments', type: :request do
             expect(response).to have_http_status(:unauthorized)
         end
     end
-   
+    describe 'appointment create endpoints' do
+        it 'return successful message if user is admin' do
+          @user.update(role: 'admin')
+          post "/api/v1/users/#{@user['id']}/appointments", params: { appointment: { date: Faker::Date.forward(days: 23),
+            user_id: @user.id,
+            therapist_id: @therapist.id } }, headers: { Authorization: @token }
+          expect(response).to have_http_status(:success)
+        end
+        it 'returns error message if token is not valid' do
+            token = nil
+            post "/api/v1/users/#{@user['id']}/appointments", params: { appointment: { date: Faker::Date.forward(days: 23),
+              user_id: @user.id,
+              therapist_id: @therapist.id } }, headers: { Authorization: token }
+            expect(response).to have_http_status(:unauthorized)
+        end
+    end
+    
 end
